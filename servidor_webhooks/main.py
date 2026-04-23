c33b0e234e005e0e309eddffd38173489a7b24de
RSolar <rsolar@kio.tech>
2026-04-23 13:29:06 -0600

    Update main.py

--- servidor_webhooks/main.py
+++ servidor_webhooks/main.py
@@ -3,8 +3,7 @@ import json
 import os
 import base64
 import requests
-
-
+from fastapi import Header
 app = FastAPI()
 
 # ----------------------------------------
@@ -105,8 +104,7 @@ async def telnyx_webhook(request: Request, token: str = None):
 # ----------------------------------------
 # Webhook Commvault
 # ----------------------------------------
-#@app.post("/commvault/webhook")
-@app.post("/commvault/webhook", methods=["POST", "GET"])
+@app.post("/commvault/webhook")
 async def commvault_webhook(request: Request, token: str = None):
     # 1. Validamos seguridad con el token que ya tienes configurado
     validate_token(token)
